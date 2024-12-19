f97f90547dd508d31db04faea5693a8df29a957c
Ranjan <ranjan@pearlorganisation.com>
2024-12-19 11:09:57 +0530

    manish first commit

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,6 +1,6 @@
 import addWatermark from "../utils/pdfWatermarker.js";
 import fs from "fs";
-
+const ttf ='../controllers/DelinaScript.ttf';
 export const uploadFile = async (req, res) => {
   //   try {
   //     const filePath = req.file.path;
@@ -22,11 +22,11 @@ export const downloadFile = async (req, res) => {
   try {
     const user = { name: "Shubham Mamgain" };
     const watermarkText = `Name: ${user.name}`;
-
+  
     const pdfUrl =
       "https://res.cloudinary.com/dapjyizvj/raw/upload/v1731995581/Bazar91/szw9i42udrcsfjifxwye.pdf";
 
-    const watermarkedPdfBytes = await addWatermark(pdfUrl, watermarkText);
+    const watermarkedPdfBytes = await addWatermark(pdfUrl, watermarkText, ttf);
 
     res.set({
       "Content-Type": "application/pdf",
--- utils/pdfWatermarker.js
+++ utils/pdfWatermarker.js
@@ -1,24 +1,80 @@
-import { PDFDocument, rgb } from "pdf-lib";
-import fetch from "node-fetch";
+import hummus from "hummus"
+import fetch from "node-fetch"
 
-const addWatermark = async (pdfUrl, watermarkText) => {
-  const pdfBuffer = await fetch(pdfUrl).then((res) => res.arrayBuffer());
 
-  const pdfDoc = await PDFDocument.load(pdfBuffer);
 
-  const pages = pdfDoc.getPages();
-  pages.forEach((page) => {
-    const { width, height } = page.getSize();
-    page.drawText(watermarkText, {
-      x: width / 4,
-      y: height / 2,
-      size: 36,
-      color: rgb(0.95, 0.1, 0.1),
-      opacity: 0.8,
-    });
-  });
+// import {
+//   PDFDocument,
+//   rgb,
+//   degrees,
+//   StandardFonts
+// } from "pdf-lib";
+// import fetch from "node-fetch";
 
-  return pdfDoc.save();
-};
+// const addWatermark = async (pdfUrl, watermarkText) => {
+//   const pdfBuffer = await fetch(pdfUrl).then((res) => res.arrayBuffer());
+
+//   const pdfDoc = await PDFDocument.load(pdfBuffer);
+
+//   const pages = pdfDoc.getPages();
+//   pages.forEach((page) => {
+//     const { width, height } = page.getSize();
+//     page.drawText(watermarkText, {
+//       x: width / 4,
+//       y: height / 2,
+//       size: 36,
+//       color: rgb(0.95, 0.1, 0.1),
+//       opacity: 0.8,
+//     });
+//   });
+
+//   return pdfDoc.save();
+// };
+
+ 
+//  const hummus = require('hummus');
+//  const fetch = require('node-fetch');
+ 
+
+ const addWatermark = async (pdfUrl, watermarkText, fontPath) => {
+   try {
+     const pdfBytes = await fetch(pdfUrl).then(res => res.buffer()); // Fetch PDF from URL
+     const pdfReader = hummus.createReader(new hummus.PDFRStreamForBuffer(pdfBytes));
+     const pdfWriter = hummus.createWriter(new hummus.PDFWStreamForBuffer());
+     const font = pdfWriter.getFontForFile(fontPath);
+
+     const pageCount = pdfReader.getPagesCount();
+     for (let i = 0; i < pageCount; i++) {
+       const originalPage = pdfReader.parsePage(i);
+       pdfWriter.createPage(originalPage);
+       const page = pdfWriter.modifyPage(i);
+       const {
+         width,
+         height
+       } = page.getSize();
+       const textWidth = font.widthOfTextAtSize(watermarkText, 36);
+       const x = (width - textWidth) / 2;
+       const y = height / 2;
+
+
+       page.writeText(watermarkText, x, y, {
+         font,
+         size: 36,
+         color: [0.95, 0.1, 0.1],
+         opacity: 0.8,
+       });
+
+
+       page.end();
+     }
+
+     pdfWriter.end();
+     return pdfWriter.toBuffer(); // Return the modified PDF buffer
+
+   } catch (error) {
+     console.error('Error adding watermark:', error);
+     throw error; // Re-throw for proper error handling in the calling function
+   }
+ };
 
 export default addWatermark;
