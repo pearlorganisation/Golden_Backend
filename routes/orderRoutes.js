979fb6ce89652ccf9a59abe1e76f5dfc91bb03e0
Saurabh Chamola <saurabh@pearlorganisation.com>
2025-02-07 11:31:48 +0530

    api for all orders

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,13 +1,17 @@
+import express, { Router } from "express";
+import {
+  createOrder,
+  getAllPurchases,
+  getPurchaseByUser,
+  verifyPayment,
+} from "../controllers/order/orderController.js";
 
+const orderRouter = express.Router();
 
-import express, { Router } from "express"
-import { createOrder, getPurchaseByUser, verifyPayment } from "../controllers/order/orderController.js"
-
-const orderRouter= express.Router()
-
-orderRouter.route("/create").post(createOrder)
-orderRouter.route("/verify").post(verifyPayment)
-orderRouter.route("/purchase").get(getPurchaseByUser)
+orderRouter.route("/create").post(createOrder);
+orderRouter.route("/verify").post(verifyPayment);
+orderRouter.route("/purchase").get(getPurchaseByUser);
+orderRouter.route("/all-orders").get(getAllPurchases);
 
 /** route for buying all the pdfs */
 
